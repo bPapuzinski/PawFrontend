bed654830d5cd167945035f4cd8fd5638bce6841
209278 <209278@edu.p.lodz.pl>
2019-11-02 02:26:09 +0100

    template of website

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header/header.js';
+import './index.css';
+
+var elements = [<Header />];
+
+ReactDOM.render(
+    elements,
+    document.getElementById('root')
+);
